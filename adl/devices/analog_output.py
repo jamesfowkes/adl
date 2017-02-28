a0522379f84674c9b979fe49b36fbe3d15b91275
James Fowkes <jamesfowkes@gmail.com>
2017-02-28 16:19:49 +0000

    Fix incorrect declaration for analog output

--- adl/devices/analog_output.py
+++ adl/devices/analog_output.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 from yapsy.IPlugin import IPlugin
 
 from adl.devices.generic_device import GenericDevice
+from adl.types import Setting
 
 class AnalogOutput(GenericDevice, namedtuple("AnalogOutput", ["name", "pin", "llimit", "hlimit"])):
 
@@ -24,13 +25,9 @@ class AnalogOutput(GenericDevice, namedtuple("AnalogOutput", ["name", "pin", "ll
 
 	@property
 	def declarations(self):
-		if self.limits:
-			return "static AnalogOutput {name} = AnalogOutput({pin}, {llow}, {lhigh});".format(
-				name=self.cname(), pin=self.pin.value, llow=self.llimit, lhigh=self.hlimit)
-		else:
-			return "static AnalogOutput {name} = AnalogOutput({pin});".format(
-				name=self.cname(), pin=self.pin.value)
-		
+		return "static AnalogOutput {name} = AnalogOutput({pin}, {llow}, {lhigh});".format(
+			name=self.cname(), pin=self.pin.value, llow=self.llimit.value, lhigh=self.hlimit.value)
+
 class AnalogOutputPlugin(IPlugin):
 	def activate(self):
 		pass
@@ -39,8 +36,8 @@ class AnalogOutputPlugin(IPlugin):
 		pass
 
 	def get(self, device):
-		llimit = device.settings.get("limits", 0)
-		hlimit = device.settings.get("limits", 255)
+		llimit = device.settings.get("limits", Setting("llimit", "", 0))
+		hlimit = device.settings.get("limits", Setting("hlimit", "", 255))
 		return AnalogOutput(device.name, device.settings["pin"], llimit, hlimit)
 
 	def set_log_level(self, level):
