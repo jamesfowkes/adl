7adf505de249074a8a410b236258a516a90ea6e9
James Fowkes <jamesfowkes@gmail.com>
2018-02-24 22:00:20 +0000

    Use DEVICE_ADDRESS typedef

--- adl/adl_code/adl.h
+++ adl/adl_code/adl.h
@@ -5,6 +5,8 @@
 static const int INVALID_ADDRESS = -1;
 static const int ADL_BOARD_ADDRESS = 0;
 
+typedef int DEVICE_ADDRESS;
+
 typedef int (*COMMAND_HANDLER)(char const * const command, char * reply);
 
 void adl_handle_any_pending_commands();
@@ -18,15 +20,15 @@ void adl_service_timer();
 bool is_digit_string(char const * s);
 
 int adl_device_count();
-DeviceBase& adl_get_device(int n);
-COMMAND_HANDLER& adl_get_command_handler(uint8_t address);
+DeviceBase& adl_get_device(DEVICE_ADDRESS address);
+COMMAND_HANDLER& adl_get_command_handler(DEVICE_ADDRESS address);
 
 class ProtocolHandlerBase
 {
 public:
 	virtual void process(char *) = 0;
 	virtual void write_reply(char * buffer, char const * const reply, uint8_t reply_length) = 0;
-	uint8_t address;
+	DEVICE_ADDRESS address;
 	char const * command;
 	
 };
