1ed466c080273618b69587c0daab9004b376adf4
James Fowkes <jamesfowkes@gmail.com>
2018-04-14 16:46:49 +0100

    Add unittest common code

---
+++ adl/unittest/Arduino.h
@@ -0,0 +1,6 @@
+#ifndef _ARDUINO_H_
+#define _ARDUINO_H_
+
+unsigned long millis();
+
+#endif
---
+++ adl/unittest/adl_defs.h
@@ -0,0 +1,8 @@
+#ifndef _ADL_DEFS_H_
+#define _ADL_DEFS_H_
+
+static const int ADL_BUFFER_SIZE = 64;
+static const int ADL_DEVICE_COUNT = 1;
+static const int ADL_PARAM_COUNT = 0;
+
+#endif
