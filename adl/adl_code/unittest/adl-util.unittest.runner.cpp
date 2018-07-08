b5548f5455e69af4c3ccfdfb0c3d2f39b1e68bda
James Fowkes <jamesfowkes@gmail.com>
2018-07-08 23:56:32 +0100

    Add utility unit tests

---
+++ adl/adl_code/unittest/adl-util.unittest.runner.cpp
@@ -0,0 +1,15 @@
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+int main()
+{
+   CppUnit::TextUi::TestRunner runner;
+
+   CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+
+   runner.addTest( registry.makeTest() );
+   runner.run();
+
+   return 0;
+}
