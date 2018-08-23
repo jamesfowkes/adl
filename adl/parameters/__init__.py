697b1e9ec0faa33d28c67fa685bf9aea27a0b05f
James Fowkes <jamesfowkes@gmail.com>
2018-08-23 08:57:58 +0100

    Fix logging architecture for params __init__

--- adl/parameters/__init__.py
+++ adl/parameters/__init__.py
@@ -10,21 +10,22 @@ parameters_plugin_manager = PluginManager()
 parameters_plugin_manager.setPluginPlaces([str(THIS_PATH)])
 parameters_plugin_manager.collectPlugins()
 
-logger = logging.getLogger(__name__)
+def get_module_logger():
+	return logging.getLogger(__name__)
 
 def activate_all():
 	for plugin in parameters_plugin_manager.getAllPlugins():
-		logger.info("Loaded parameter plugin '%s'", plugin.name)
+		get_module_logger().info("Loaded parameter plugin '%s'", plugin.name)
 		parameters_plugin_manager.activatePluginByName(plugin.name)
 
 def get_single_parameter(parameter):
-	logger.info("Trying to load parameter '%s' (%s)", parameter.name, parameter.type)
+	get_module_logger().info("Trying to load parameter '%s' (%s)", parameter.name, parameter.type)
 	return parameters_plugin_manager.getPluginByName(parameter.type).plugin_object.get(parameter)
 
 def get(parameters):
 	return [get_single_parameter(parameter) for parameter in parameters]
 	
 def set_log_level(level):
-	logger.setLevel(level)
+	get_module_logger().setLevel(level)
 	for plugin in parameters_plugin_manager.getAllPlugins():
 		plugin.plugin_object.set_log_level(level)
