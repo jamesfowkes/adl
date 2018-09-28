9724eacf26b0d628812a8b4f41fe345507c8cd17
James Fowkes <jamesfowkes@gmail.com>
2018-09-28 13:52:17 +0100

    Add new modules folder - needs integration into ADL

---
+++ adl/modules/oneshot-timer/adl-oneshot-timer.h
@@ -0,0 +1,20 @@
+#ifndef _ADL_ONESHOT_H_
+#define _ADL_ONESHOT_H_
+
+class ADLOneShotTimer
+{
+public:
+	ADLOneShotTimer(uint16_t period);
+	void start();
+	bool check();
+	void reset();
+	bool check_and_reset();
+	bool check_and_restart();
+	bool is_running();
+private:
+	unsigned long m_start;
+	uint16_t m_period;
+	bool m_running;
+};
+
+#endif
