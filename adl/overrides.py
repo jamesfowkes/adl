faaa660b7c6f479e26c0f4c3276b359dce5446e1
James Fowkes <jamesfowkes@gmail.com>
2018-10-11 07:31:17 +0100

    Add FQBN override

--- adl/overrides.py
+++ adl/overrides.py
@@ -8,8 +8,16 @@ def apply_boardtype_override(new_boardtype, board):
 	board = board._replace(type=new_boardtype)
 	return board
 
+def apply_fqbn_override(new_fqbn, board):
+	get_module_logger().info("Overriding fqbn '{}' with '{}'".format(board.attrs.get("fqbn", "default"), new_fqbn))
+	new_attrs = board.attrs.copy()
+	new_attrs["fqbn"] = new_fqbn
+	board = board._replace(attrs=new_attrs)
+	return board
+
 OVERRIDE_FUNCTION_MAP = {
-	"boardtype": apply_boardtype_override
+	"boardtype": apply_boardtype_override,
+	"fqbn": apply_fqbn_override
 }
 
 def apply_overrides(overrides, board):
