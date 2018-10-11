b86da706fe0dacda1a2852de5eeaff9951b4ad7b
James Fowkes <jamesfowkes@gmail.com>
2018-10-11 07:29:00 +0100

    Add logging to CLI interface

--- arduino_cli_interface.py
+++ arduino_cli_interface.py
@@ -1,9 +1,19 @@
 import subprocess
+import logging
+from pathlib import Path
+
+THIS_PATH = Path(__file__).parent
+
+def get_module_logger():
+	return logging.getLogger(__name__)
 
 def verify(board, sketch_path):
 	args = ["arduino-cli", "compile", "--fqbn", board.fqbn, str(sketch_path)]
-	subprocess.run(args)
+	result = subprocess.run(args)
+	get_module_logger().info("Verify: '{}'".format(" ".join(result.args)))
 
 def upload(board, sketch_path, port):
 	args = ["arduino-cli", "upload", "-p", port, "--fqbn", board.fqbn, str(sketch_path)]
-	subprocess.run(args)
+	result = subprocess.run(args)
+	get_module_logger().info("Upload: '{}'".format(" ".join(result.args)))
+
