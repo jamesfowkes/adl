0302c2ba346927b49ca25e1820c3b08976c21aed
James Fowkes <jamesfowkes@gmail.com>
2018-10-16 12:46:31 +0100

    Add required and default settings to debounced input

--- adl/devices/debounced-input.device-plugin/debounced_input.py
+++ adl/devices/debounced-input.device-plugin/debounced_input.py
@@ -7,7 +7,7 @@ from collections import namedtuple
 
 from yapsy.IPlugin import IPlugin
 
-from adl.types import DeviceSource, DeviceInclude, LibraryInclude
+from adl.types import DeviceSource, DeviceInclude, LibraryInclude, Setting
 from adl.devices.generic_device import GenericDevice
 
 THIS_PATH = Path(__file__).parent
@@ -38,6 +38,9 @@ class DebouncedInput(GenericDevice, namedtuple("DebouncedInput", ["name", "pin",
 			name=self.cname(), pin=self.pin.value, debounce_time=self.debounce_time.value, pullup=self.pullup.value)
 
 class TimedOnOffPlugin(IPlugin):
+
+	REQUIRED_SETTINGS = ["pin"]
+
 	def activate(self):
 		pass
 
@@ -45,8 +48,12 @@ class TimedOnOffPlugin(IPlugin):
 		pass
 
 	def get(self, device):
+
+		debounce_time = device.settings.get("debounce_time", Setting("debounce_time","","50"))
+		pullup = device.settings.get("pullup", Setting("pullup","","true"))
+
 		return DebouncedInput(device.name, 
-			device.settings["pin"], device.settings["debounce_time"], device.settings["pullup"])
+			device.settings["pin"], debounce_time, pullup)
 
 	def set_log_level(self, level):
 		logging.getLogger(__name__).setLevel(level)
