a4c2cbefabe46539e68857875350cdbbe7d5fe1a
James Fowkes <jamesfowkes@gmail.com>
2018-11-05 08:48:35 +0000

    Update unit tests

--- adl/adl_code/protocols/url/unittest/url.unittest.cpp
+++ adl/adl_code/protocols/url/unittest/url.unittest.cpp
@@ -14,9 +14,9 @@ class URLProtocolTest : public CppUnit::TestFixture {
 
     CPPUNIT_TEST_SUITE(URLProtocolTest);
     CPPUNIT_TEST(testValidBoardDeviceCommandIsProcessed);
+    CPPUNIT_TEST(testValidParameterCommandIsProcessed);
     CPPUNIT_TEST_SUITE_END();
 
-  
     void testValidBoardDeviceCommandIsProcessed() {
         char valid_command[] = "/device/00/R";
         ProtocolHandler s_protocol_handler;
@@ -26,7 +26,18 @@ class URLProtocolTest : public CppUnit::TestFixture {
         CPPUNIT_ASSERT_EQUAL(0, s_protocol_handler.last_address);
         CPPUNIT_ASSERT_EQUAL(ADDRESS_TYPE_DEVICE, s_protocol_handler.last_address_type);
         CPPUNIT_ASSERT_EQUAL(std::string("R"), std::string(s_protocol_handler.command));
-  }
+    }
+
+    void testValidParameterCommandIsProcessed() {
+        char valid_command[] = "/param/01/S128";
+        ProtocolHandler s_protocol_handler;
+
+        s_protocol_handler.process(valid_command);
+
+        CPPUNIT_ASSERT_EQUAL(1, s_protocol_handler.last_address);
+        CPPUNIT_ASSERT_EQUAL(ADDRESS_TYPE_PARAM, s_protocol_handler.last_address_type);
+        CPPUNIT_ASSERT_EQUAL(std::string("S128"), std::string(s_protocol_handler.command));
+    }
 };
 
 int main()
