a60c61556a9764e2f61e27a6b22b2e2088651610
James Fowkes <jamesfowkes@gmail.com>
2018-11-07 07:14:01 +0000

    Add setPixelColor and show functions to Neopixel class

--- adl/devices/adafruit-neopixel.device-plugin/adafruit-neopixel-adl.h
+++ adl/devices/adafruit-neopixel.device-plugin/adafruit-neopixel-adl.h
@@ -13,6 +13,10 @@ public:
     void set_pixels(uint8_t range_min, uint8_t range_max, uint8_t r, uint8_t g, uint8_t b);
     void set_pixels(uint8_t range_min, uint8_t range_max, const uint8_t rgb[3]);
     uint8_t npixels();
+    void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b);
+    void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+    void setPixelColor(uint16_t n, uint32_t c);
+    void show();
     
 private:
     int handle_command(char const * const command, char * reply);
