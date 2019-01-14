bd502117c612975a968c6005c6372e15163937a6
James Fowkes <jamesfowkes@gmail.com>
2019-01-14 09:29:41 +0000

    Unit tests pass with application header

---
+++ adl/unittest/adl-application.h
@@ -0,0 +1,14 @@
+#ifndef _ADL_APPLICATION_H_
+#define _ADL_APPLICATION_H_
+
+typedef struct _adl_devices_struct
+{
+
+} adl_devices_struct;
+
+typedef struct _adl_params_struct
+{
+
+} adl_params_struct;
+
+#endif
