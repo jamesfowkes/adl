cd10f6e19f79dce870577f7d66ea7b24155b484d
James Fowkes <jamesfowkes@gmail.com>
2019-08-20 01:12:23 +0100

    Add WS2801 device. Closes #43

---
+++ raat/devices/WS2801.device-plugin/WS2801.cpp
@@ -0,0 +1,190 @@
+#include <Adafruit_WS2801.h>
+
+#include "raat.hpp"
+
+#include "WS2801.hpp"
+
+/*
+ * Class Private Functions
+ */
+
+static bool valid_rgb_value(int32_t val)
+{
+    return (val >= 0) && (val <= 255);
+}
+
+static bool valid_rgb_values(int32_t(&rgb)[3])
+{
+    return valid_rgb_value(rgb[0]) && valid_rgb_value(rgb[1]) && valid_rgb_value(rgb[2]);
+}
+
+/*
+ * Class RAAT Functions
+ */
+
+void WS2801::set_pixels(uint8_t range_min, uint8_t range_max, RGBParam<uint8_t>& param)
+{
+    uint8_t rgb[3];
+    param.get(rgb);
+    this->setPixelColor(range_min, range_max, (uint8_t)rgb[0], (uint8_t)rgb[1], (uint8_t)rgb[2]);
+}
+
+void WS2801::set_pixels(uint8_t range_min, uint8_t range_max, uint8_t r, uint8_t g, uint8_t b)
+{
+    Serial.println(range_min);
+    Serial.println(range_max);
+    if (range_min < range_max)
+    {
+        for (uint8_t i=range_min; i<range_max+1; i++)
+        {
+            Serial.print(i);
+            Serial.print(",");
+            Serial.print(r);
+            Serial.print(",");
+            Serial.print(g);
+            Serial.print(",");
+            Serial.println(b);
+            m_pixels.setPixelColor(i, r, g, b);
+        }
+    }
+}
+
+void WS2801::set_pixels(uint8_t range_min, uint8_t range_max, const uint8_t rgb[3])
+{
+    this->set_pixels(range_min, range_max, rgb[0], rgb[1], rgb[2]);
+}
+
+int WS2801::handle_command(char const * const command, char * reply)
+{
+    int32_t rgb[3];
+
+    int32_t range_min;
+    int32_t range_max;
+
+    char * end_of_range = NULL;
+
+    bool valid_range = raat_convert_numeric_range(command, range_min, range_max, &end_of_range);
+    valid_range &= range_min < this->m_npixels;
+    valid_range &= range_max < this->m_npixels;
+
+    if (valid_range && (*end_of_range == ':'))
+    {
+        end_of_range++;        
+        uint8_t parsed_count = raat_parse_comma_separated_numerics(end_of_range, rgb);
+
+        if (parsed_count == 3)
+        {
+            if (valid_rgb_values(rgb))
+            {
+                this->set_pixels(range_min, range_max, (uint8_t)rgb[0], (uint8_t)rgb[1], (uint8_t)rgb[2]);
+                this->show();
+                return sprintf(reply, "%u-%u: %u,%u,%u",
+                    (uint8_t)range_min, (uint8_t)range_max,
+                    (uint8_t)rgb[0], (uint8_t)rgb[1], (uint8_t)rgb[2]);
+                
+            }
+            else
+            {
+                return raat_msg_invalid_values(reply);
+            }
+        }
+        else
+        {
+            return raat_msg_wrong_number_of_values(reply, parsed_count, end_of_range);
+        }
+    }
+    else
+    {
+        return raat_msg_invalid_range(reply);
+    }
+}
+
+void WS2801::setPixelColor(uint16_t n, RGBParam<uint8_t>& param)
+{
+    uint8_t rgb[3];
+    param.get(rgb);
+    this->setPixelColor(n, (uint8_t)rgb[0], (uint8_t)rgb[1], (uint8_t)rgb[2]);
+}
+
+void WS2801::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b)
+{
+    m_pixels.setPixelColor(n, r, g, b);
+}
+
+void WS2801::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w)
+{
+    m_pixels.setPixelColor(n, r, g, b, w);
+}
+
+void WS2801::setPixelColor(uint16_t n, uint32_t c)
+{
+    m_pixels.setPixelColor(n, c);
+}
+
+void WS2801::show()
+{
+    m_pixels.show();   
+}
+
+void WS2801::clear()
+{
+    for (uint8_t i=0; i<m_npixels; i++)
+    {
+        m_pixels.setPixelColor(i, 0, 0, 0);
+    }
+    m_pixels.show();
+}
+
+/*
+ * Class RAAT Functions
+ */
+
+WS2801::WS2801(uint8_t data_pin, uint8_t clock_pin, uint16_t npixels) : 
+    m_pixels(npixels, data_pin, clock_pin), m_npixels(npixels)
+{
+    m_pixels.begin();
+}
+
+void WS2801::reset()
+{
+    this->clear();
+    m_pixels.show();
+}
+
+void WS2801::tick()
+{
+
+}
+
+void WS2801::setup()
+{
+    this->reset();
+}
+
+uint16_t WS2801::command_handler(char const * const command, char * reply)
+{
+    int reply_length;
+
+    if (command[0] == 'R')
+    {
+        this->reset();
+        strcpy(reply, "ROK");
+        reply_length = strlen(reply);
+    }
+    else
+    {
+        reply_length = handle_command(command, reply);
+    }
+
+    return reply_length;
+}
+
+Adafruit_WS2801& WS2801::pixels()
+{
+    return m_pixels;
+}
+
+uint8_t WS2801::npixels()
+{
+    return m_npixels;
+}
---
+++ raat/devices/WS2801.device-plugin/WS2801.py
@@ -0,0 +1,72 @@
+import logging
+
+from pathlib import Path
+
+from collections import namedtuple
+
+from yapsy.IPlugin import IPlugin
+
+from raat.types import LibraryInclude, DeviceSource, DeviceInclude
+
+from raat.parameters.rgb.rgb import RGBParam
+from raat.devices.generic_device import GenericDevice, GenericDevicePlugin
+from raat.types import Setting
+
+THIS_PATH = Path(__file__).parent
+
+
+class WS2801(GenericDevice, namedtuple("WS2801", ["name", "data_pin", "clock_pin", "npixels"])):
+
+    __slots__ = ()
+
+    sources = (DeviceSource(THIS_PATH, "WS2801.cpp"), )
+    sources += RGBParam.sources
+
+    includes = (
+        DeviceInclude(THIS_PATH, "WS2801.hpp"),
+        LibraryInclude("Adafruit_WS2801.h")
+    )
+    includes += RGBParam.includes
+
+    @property
+    def setup(self):
+        return "{name}.setup();".format(name=self.cname())
+
+    @property
+    def directory(self):
+        return THIS_PATH
+
+    @property
+    def required_libraries(self):
+        return ["Adafruit WS2801"]
+
+    @property
+    def declarations(self):
+        return "static WS2801 {name} = WS2801({data_pin}, {clock_pin}, {npixels});".format(
+            name=self.cname(), data_pin=self.data_pin.value, clock_pin=self.clock_pin.value, 
+            npixels=self.npixels.value)
+
+
+class WS2801Plugin(IPlugin, GenericDevicePlugin):
+
+    REQUIRED_SETTINGS = ["data_pin", "clock_pin", "npixels"]
+
+    device_class = WS2801
+
+    def activate(self):
+        pass
+
+    def deactivate(self):
+        pass
+
+    def get(self, device):
+        self.verify_settings(device)
+
+        return WS2801(device.name,
+            device.settings["data_pin"],
+            device.settings["clock_pin"],
+            device.settings["npixels"],
+        )
+
+    def set_log_level(self, level):
+        logging.getLogger(__name__).setLevel(level)
