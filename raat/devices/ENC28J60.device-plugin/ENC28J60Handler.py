ac2d9dccb5ff636a8dc6f7a8a45e02fc9a737c40
James Fowkes <jamesfowkes@gmail.com>
2019-08-30 21:09:44 +0100

    Add --nowait option to ENC28J60 device handler

--- raat/devices/ENC28J60.device-plugin/ENC28J60Handler.py
+++ raat/devices/ENC28J60.device-plugin/ENC28J60Handler.py
@@ -1,7 +1,10 @@
 """ ENC28J60Handler.py
 
 Usage:
-    ENC28J60Handler.py <port> <device_number> [--baud=<baud>] [--ip=<ip>] [--mac=<mac>] [--gateway=<gateway>]
+    ENC28J60Handler.py <port> <device_number> [--baud=<baud>] [--ip=<ip>] [--mac=<mac>] [--gateway=<gateway>] [--nowait]
+
+Options:
+    --nowait  Do not wait to receive lines from the device
 
 """
 
@@ -39,12 +42,14 @@ if __name__ == "__main__":
 
     args = docopt.docopt(__doc__)
 
+    print(args)
     baudrate = args["--baud"] or 115200
     device_number = int(args["<device_number>"])
 
     with serial.Serial(port=args["<port>"], baudrate=baudrate, timeout=1) as ser:
-        read_until_condition(ser, lambda l: len(l) > 0, line_printer)
-        read_until_condition(ser, lambda l: len(l) == 0, line_printer)
+        if not args["--nowait"] :
+            read_until_condition(ser, lambda l: len(l) > 0, line_printer)
+            read_until_condition(ser, lambda l: len(l) == 0, line_printer)
 
         if args["--ip"] is not None:
             cmd = "/device/{:02d}/SIP{}".format(device_number, args["--ip"])
