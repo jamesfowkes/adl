b52bda98e433dacb99a611476e9418465be3c164
James Fowkes <jamesfowkes@gmail.com>
2020-01-16 18:46:06 +0000

    Digital Output uses timer module for toggling

--- raat/devices/digital-output.device-plugin/digital-output.hpp
+++ raat/devices/digital-output.device-plugin/digital-output.hpp
@@ -1,11 +1,14 @@
 #ifndef _DIGITAL_OUTPUT_H_
 #define _DIGITAL_OUTPUT_H_
 
+#include "raat-oneshot-timer.hpp"
+
 typedef enum startup_state
 {
 	STARTUP_STATE_LOW,
 	STARTUP_STATE_HIGH,
-	STARTUP_STATE_TRISTATE
+	STARTUP_STATE_TRISTATE,
+    STARTUP_STATE_PU
 } estartup_state;
 
 class DigitalOutput : public DeviceBase
@@ -20,19 +23,16 @@ public:
     void set(bool on, uint16_t timeout=0);
     void toggle(uint16_t timeout=0);
     bool state(void);
-    void tristate();
+    void tristate(bool pullup);
 
 private:
 
-    struct
-    {
-        uint16_t time;
-        bool active;
-    } m_timeout;
-
     int m_pin;
-    void start_timeout(uint16_t timeout);
+    
     estartup_state m_startup_state;
+    RAATOneShotTimer m_timeout;
+
+    void start_timeout(uint16_t timeout);
 };
 
 #endif
